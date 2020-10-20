221ab4678e0a711882b2f0a6209e7ad5099e0b6a
Marc Ferraro <marc.ferraro1@gmail.com>
2020-10-20 05:09:08 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -7,11 +7,9 @@ end
 
 def state_log(star_date)
   "Captain's Log, star date #{star_date}."
-  binding.pry
 end
 
 def engage
   puts state_log(date)
   date = generate_star_date
-  binding.pry
 end
