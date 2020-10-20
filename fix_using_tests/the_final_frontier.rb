a407aa1b9f4082f314922a9a697d4b6fe35d2f7e
Marc Ferraro <marc.ferraro1@gmail.com>
2020-10-20 05:10:24 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 def generate_star_date
   (rand(100000) + 400000) / 10.0
-  
+   binding.pry
 end
 
 
@@ -13,5 +13,5 @@ end
 def engage
   date = generate_star_date
   puts state_log(date)
-  binding.pry
+ 
 end
